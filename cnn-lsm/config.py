5b350e132a7e8cb6687a6c4f5c743fb400a1e9cf
00yubo1128 <99158269+00yubo1128@users.noreply.github.com>
2022-04-20 21:22:12 +0800

    Update config.py

--- cnn-lsm/config.py
+++ cnn-lsm/config.py
@@ -1,2 +1,5 @@
-MODEL_PATH = './autodl-tmp/data/model_resnet_singleobj.h5'
-PASCAL_PATH = '/root/autodl-tmp/VOC2012'
+############ 用于记录训练好的CNN模型架构和下载的PASCAL VOC2012数据集存放的路径 ##############
+
+
+MODEL_PATH = './autodl-tmp/data/model_resnet_singleobj.h5' #训练好的CNN模型架构存放路径
+PASCAL_PATH = '/root/autodl-tmp/VOC2012' #下载的PASCAL VOC2012数据集存放路径
