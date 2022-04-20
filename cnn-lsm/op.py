e4656f9205faaecf06b0f2740e99916c0cfbb643
00yubo1128 <99158269+00yubo1128@users.noreply.github.com>
2022-04-20 21:41:44 +0800

    Update op.py

--- cnn-lsm/op.py
+++ cnn-lsm/op.py
@@ -1,3 +1,6 @@
+########## 定义了GAC模型分割中会用到的一些函数 ##############
+
+
 import numpy as np
 
 
@@ -5,6 +8,7 @@ def dot(x, y, axis=0):
     return np.sum(x * y, axis=axis)
 
 
+#求x的数值梯度
 def grad(x):
     return np.array(np.gradient(x))
 
@@ -13,6 +17,7 @@ def norm(x, axis=0):
     return np.sqrt(np.sum(np.square(x), axis=axis))
 
 
+#定义求曲率函数
 def curvature(f):
     fy, fx = grad(f)
     norm = np.sqrt(fx**2 + fy**2)
@@ -21,6 +26,7 @@ def curvature(f):
     return div(Nx, Ny)
 
 
+#定义求散度函数
 def div(fx, fy):
     fyy, fyx = grad(fy)
     fxy, fxx = grad(fx)
